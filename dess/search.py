c4984873eec53fa017cdb3ae6e800a360edf9d69
Shishir Aravindan <shishir.aravindan@gmail.com>
2024-09-26 01:01:46 -0400

    search.py refactor: clean-up and docs

--- dess/search.py
+++ dess/search.py
@@ -1,15 +1,65 @@
-import selenium
+"""
+Provides functionality to create web drivers for Chrome and Firefox using Selenium, perform Google 
+searches, and parse the search results.
+
+Dependencies:
+- Selenium: A browser automation framework. Ensure that the appropriate browser driver (ChromeDriver or GeckoDriver) 
+is installed and accessible in your PATH.
+"""
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as FireFoxOptions
 import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
+def create_chrome_driver():
+    """
+    Initializes and returns a Chrome web driver with specified options.
+
+    Returns:
+        webdriver.Chrome: An instance of the Chrome web driver configured with headless options.
+    """
+    options = Options()
+    options.add_argument("--headless")
+    #options.add_experimental_option("detach", True)
+    service = Service('/opt/homebrew/bin/chromedriver')
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
+
+def create_firefox_driver():
+    """
+    Initializes and returns a Firefox web driver with specified options.
+
+    Returns:
+        webdriver.Firefox: An instance of the Chrome web driver configured with headless options.
+    """
+    # Set up Selenium options
+    options = FireFoxOptions()
+    options.set_preference("dom.popup_maximum", 0)
+    options.set_preference("privacy.popups.disable_from_plugins", 3)
+    # options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options)
+    return driver
 
 
-def search_google(driver,name: str, university: str,snapshots:int):
+def search_google(driver: webdriver,name: str, university: str,snapshots:int):
+    """
+    Performs a Google search for the given name and university, and retrieves specified snapshots of the search results.
+
+    Args:
+        driver (webdriver): The web driver instance to use for the search.
+        name (str): The name to search for.
+        university (str): The university to include in the search query.
+        snapshots (int): The number of result snapshots to retrieve.
+
+    Returns:
+        list: A list containing the parsed text of the search result snapshots.
+
+    Raises:
+        Exception: Raises an exception if there is an error during the search or result retrieval.
+    """
     search_query = f'"{name}" {university}'
     google_url = f"https://www.google.com/search?q={search_query.replace(' ', '+')}"
     driver.get(google_url)
@@ -39,15 +89,11 @@ def search_google(driver,name: str, university: str,snapshots:int):
                 continue
 
             index += 1
-        #print(f'Feature vector: {feature_vector}')
         return feature_vector
 
     except Exception as e:
         print(f'Error while scraping', e)
         # driver.quit()
-
-    # Close the driver
-    #driver.quit()
 
 
 def parse_text(text: str):
