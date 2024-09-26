5af54c5780e67c703eb0d86e3452e30bc89b5eff
AkhilChoraria <chorariaakhil03@gmail.com>
2024-09-25 23:03:55 -0400

    Add starter code for scraping

--- dess/search.py
+++ dess/search.py
@@ -9,25 +9,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def search_google(name: str, university: str,snapshots:int):
-    # Have options
-    options = Options()
-    options.headless = True
-    options.add_experimental_option("detach", True)
-    service = Service('/opt/homebrew/bin/chromedriver')
-
-    # pass options to driver
-    driver = webdriver.Chrome(service=service, options=options)
+def search_google(driver,name: str, university: str,snapshots:int):
     search_query = f'"{name}" {university}'
     google_url = f"https://www.google.com/search?q={search_query.replace(' ', '+')}"
-    # print(f'Using URL = {google_url}')
-
     driver.get(google_url)
 
-    time.sleep(3)  # Adjust sleep time as needed depending on page load time
+    time.sleep(2)  
 
-    # Find the first 3 search results
+    # Find the first snapshot search results
     try:
+        #This will contain the raw text from the search results
+        feature_vector = []
         results = driver.find_elements(By.XPATH, '//div[@class="MjjYud"]')
         no_of_wanted_results = snapshots
         completed = 0
@@ -40,13 +32,15 @@ def search_google(name: str, university: str,snapshots:int):
                     index += 1
                     continue
                 div_text = div_element.text
-                parse_text(div_text)
+                feature_vector.append(parse_text(div_text))
                 completed += 1
             except:
                 index += 1
                 continue
 
             index += 1
+        #print(f'Feature vector: {feature_vector}')
+        return feature_vector
 
     except Exception as e:
         print(f'Error while scraping', e)
@@ -59,9 +53,9 @@ def search_google(name: str, university: str,snapshots:int):
 def parse_text(text: str):
     text = text.split('\n')
     # use enumerate
-    for idx, t in enumerate(text):
-        print(f'Index {idx}, text: {t}')
-
+    # for idx, t in enumerate(text):
+    #     #print(f'Index {idx}, text: {t}')
+    return text[-1].strip()
 
 if __name__ == '__main__':
-    search_google("Diana Raffman", "University of Toronto",4)
+    search_google("Tyler Holden", "University of Toronto",4)
